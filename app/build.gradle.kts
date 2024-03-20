f53bdd59fe00915649eb934108d5ae86c5c9172c
Tom <tom@saudeagora.com.vc>
2024-03-20 10:26:07 -0300

    changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,21 +12,6 @@ application {
   mainClass.set("com.example.AppStart")
 }
 
-tasks.withType<JavaExec> {
-  jvmArgs(
-    "-javaagent:../tools/otel-agent.jar",
-    "-Dotel.service.name=app-java",
-    "-Dotel.logs.exporter=otlp",
-    "-Dotel.metrics.exporter=otlp",
-    "-Dotel.traces.exporter=otlp",
-    "-Dotel.traces.sampler=always_on"
-    // "-Dotel.exporter.otlp.metrics.endpoint=http://localhost:9090/api/v1/write",
-    // "-Dotel.exporter.otlp.logs.endpoint=http://localhost:3100/api/prom/push",
-    // "-Dotel.exporter.otlp.traces.endpoint=http://localhost:4317",
-    // "-Dotel.exporter.otlp.endpoint=http://localhost:4317",
-  )
-}
-
 tasks.withType<Jar> {
   manifest {
     attributes["Main-Class"] = "com.example.AppStart"
@@ -34,18 +19,20 @@ tasks.withType<Jar> {
 }
 
 dependencies {
-  // Componentes HTTP
-  implementation("org.apache.httpcomponents:fluent-hc:4.5.13")
+  // OpenTelemetry
+  implementation("io.opentelemetry:opentelemetry-exporter-otlp:1.36.0")
 
   // Prometheus
   implementation("io.prometheus:prometheus-metrics-core:1.1.0")
   implementation("io.prometheus:prometheus-metrics-exporter-httpserver:1.1.0")
   implementation("io.prometheus:prometheus-metrics-instrumentation-jvm:1.1.0")
   implementation("io.prometheus:prometheus-metrics-exporter-opentelemetry:1.1.0")
-  implementation("io.micrometer:micrometer-registry-prometheus:1.12.4")
 
   // Logs
   implementation("org.slf4j:slf4j-api:2.0.12")
   implementation("ch.qos.logback:logback-core:1.5.0")
   implementation("ch.qos.logback:logback-classic:1.5.0")
+
+  // Componentes HTTP
+  implementation("org.apache.httpcomponents:fluent-hc:4.5.13")
 }

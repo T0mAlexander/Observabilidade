6e58f9c1596a1e54f0a37d7b3c64831214da2717
Tom <tom@saudeagora.com.vc>
2024-03-20 21:58:50 -0300

    changes

--- app/src/main/java/com/example/app/TracingConfig.java
+++ app/src/main/java/com/example/app/TracingConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class OpenTelemetryConfig {
+public class TracingConfig {
   @Bean
   public OpenTelemetryAgentSpanContextSupplier OTelAgentSupplier() {
     return new OpenTelemetryAgentSpanContextSupplier();
